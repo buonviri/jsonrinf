cc4503bfb34ac549150bc2c04d252f7ff630b5f4
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2024-05-21 23:01:32 -0400

    first draft of build options script

---
+++ BuildOptions.py
@@ -0,0 +1,36 @@
+import os
+import ast
+import pyperclip
+
+suppress_all = True
+
+
+def GetAttr(attrlist, attrname):
+    for keyvalue in attrlist:
+        if keyvalue[0] == attrname:
+            return keyvalue[1]
+    return ''
+# End
+
+
+target = os.path.basename(__file__).split('.')[0]  # get script name without extension
+
+info_text = pyperclip.paste()
+info = ast.literal_eval(info_text)
+
+clip = ''
+for refdes in info['comps']:
+    if 'attributes' in info['comps'][refdes]:
+            a = GetAttr(info['comps'][refdes]['attributes'], target)
+            if a == '':
+                pass  # attribute not found, or value was blank
+            elif suppress_all and a == 'ALL':
+                pass  # doesn't really do anything anyway
+            else:
+                print(refdes + ": " + a)
+                clip = clip + refdes + ',' + a + '\n'
+pyperclip.copy(clip)
+
+print()
+os.system("PAUSE")
+# EOF
